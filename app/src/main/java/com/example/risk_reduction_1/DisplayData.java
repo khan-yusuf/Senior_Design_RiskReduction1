9ebf81e2e698d388c9e3ae1964e8d1333e511919
Stephen Petrick <stephenp1912@gmail.com>
2022-04-03 21:37:13 -0500

    Print ArrayList successfully
    Print an array list on one line with commas separating each value. Currently still includes the original text in the xml file.
    Have two methods of printing. Commented out version on lines 38-39 prints each element on one line. Used an alternate one, as mentioned above.
    
    Tried to move printing functionality to DisplayData class, but seems like you may not be able to access the Views of another file.
    Instead, may need to be a "Fragment" of an Activity in order to access its views.
    
    Implementing DisplayData as a Fragment is marked as a TODO item.

---
+++ app/src/main/java/com/example/risk_reduction_1/DisplayData.java
@@ -0,0 +1,19 @@
+package com.example.risk_reduction_1;
+
+import android.service.autofill.TextValueSanitizer;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class DisplayData {
+
+    private TextView displayData;
+
+    /*
+     *TODO
+     * Implement this class as a "Fragment"
+     */
+    public DisplayData(ArrayList<String> inputDataToDisplay){
+
+    }
+}
--- app/src/main/java/com/example/risk_reduction_1/MainActivity.java
+++ app/src/main/java/com/example/risk_reduction_1/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.risk_reduction_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView displayDataAsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create intent to start another activity (currently used for uncreated activity)
+//        Intent display_data_intent = new Intent(this, DisplayData.class);
+//        startActivity(display_data_intent);
+        ArrayList<String> testArrayList = new ArrayList<String>(10);
+        //fill test array with numbers for test printing
+        for (int i = 0; i < 10; i++){
+            testArrayList.add(Integer.toString(i));
+        }
+
+        //Links this java file to "activity_main.xml"
+        setContentView(R.layout.activity_main);
+
+        //Links our pointer to the TextView object "display_data"
+        //in the "activity_main.xml" file
+        displayDataAsText = findViewById(R.id.display_data);
+
+        for (int i=0; i<testArrayList.size(); i++){
+            //Display each element on a separate line
+            /*displayDataAsText.append(testArrayList.get(i));
+            displayDataAsText.append("\n");*/
+
+            //Print each element on one line
+            displayDataAsText.setText(displayDataAsText.getText() + " " + testArrayList.get(i)
+                                      + ", ");
+        }
+
+//        DisplayData displayData = new DisplayData(testArrayList);
     }
 }
