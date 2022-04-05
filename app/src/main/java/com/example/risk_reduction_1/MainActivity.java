c63c007cb41c4179732408e844555ef83426037a
Tianyue Shi <shijustin032@gmail.com>
2022-04-05 16:33:28 -0500

    comments

--- app/src/main/java/com/example/risk_reduction_1/MainActivity.java
+++ app/src/main/java/com/example/risk_reduction_1/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+//import com.jjoe64.graphview.GraphView;
+//import com.jjoe64.graphview.series.DataPoint;
+//import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView displayDataAsText;
+//    GraphView graphView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +48,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
 //        DisplayData displayData = new DisplayData(testArrayList);
+
+
+//        // on below line we are initializing our graph view.
+//        graphView = findViewById(R.id.idGraphView);
+//
+//        // on below line we are adding data to our graph view.
+//        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+//                // on below line we are adding
+//                // each point on our x and y axis.
+//                new DataPoint(0, 1),
+//                new DataPoint(1, 3),
+//                new DataPoint(2, 4),
+//                new DataPoint(3, 9),
+//                new DataPoint(4, 6),
+//                new DataPoint(5, 3),
+//                new DataPoint(6, 6),
+//                new DataPoint(7, 1),
+//                new DataPoint(8, 2)
+//        });
+//
+//        // after adding data to our line graph series.
+//        // on below line we are setting
+//        // title for our graph view.
+//        graphView.setTitle("My Graph View");
+//
+//        // on below line we are setting
+//        // text color to our graph view.
+//        graphView.setTitleColor(R.color.purple_200);
+//
+//        // on below line we are setting
+//        // our title text size.
+//        graphView.setTitleTextSize(18);
+//
+//        // on below line we are adding
+//        // data series to our graph view.
+//        graphView.addSeries(series);
     }
 }
